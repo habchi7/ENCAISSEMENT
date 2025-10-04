3e4edd0375fda90fd902326a093463e58c46536c
habchi7 <69268077+habchi7@users.noreply.github.com>
2025-10-04 16:03:11 +0100

    Update ENC.js

--- ENC.js
+++ ENC.js
@@ -824,7 +824,7 @@ function clickGeneral() {
         if (conditionMet && insertButton()) {
             clearInterval(interval);
         }
-    }, 21000);
+    }, 300);
   
 /////////////////////////////////////////////////////////////////////////////////////
   
